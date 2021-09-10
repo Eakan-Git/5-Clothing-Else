b7c0a63a0cee726219b659805ee1327848adb543
Eakan <kuutruong@gmail.com>
2021-09-10 16:27:25 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# 5-Clothing-Else
---
+++ config/constants.php
@@ -0,0 +1,12 @@
+<?php
+
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASSWORD', '');
+define('DATABASE_NAME', '5clothing');
+
+define('CURRENCY', '$');
+
+
+
+?>
